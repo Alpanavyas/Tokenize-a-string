9681e816b48d79a98dada77ab2b6df976cecc2f2
Alpana Vyas <Vyas.alpana10@gmail.com>
2021-10-12 23:13:24 +0530

    Create tokenize.py

---
+++ tokenize.py
@@ -0,0 +1,15 @@
+#"""Tokenizing a string and counting unique words.""""
+text = ('this is sample text with several words '
+       'this is more sample text with some different words')
+word_counts = {}
+# count occurrences of each unique word
+for word in text.split():
+    word = word.upper()
+    if word in word_counts:
+        word_counts[word] += 1  # update existing key-value pair
+    else:
+        word_counts[word] = 1  # insert new key-value pair
+print(f'{"Word":<12}COUNT')
+for word,count in sorted(word_counts.items()):
+    print(f'{word:<12}{count}')
+print('\nNumber of unique words:',len(word_counts))
